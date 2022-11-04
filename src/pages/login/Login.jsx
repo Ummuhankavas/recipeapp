943d9d606355d5509507e7476c0cf64b4080220b
UmmuhanKavas <ummuhankavas@gmail.com>
2022-11-04 12:53:02 +0100

    Login and user added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,34 @@
-import LoginStyle from './Login.style';
+import {FormContainer, Header, LoginContainer, StyledForm, StyledImg, StyledInput, StyledButton} from './Login.style';
+import mealSvg from '../../assets/meal.svg'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: 'Ahmet',
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem('user', JSON.stringify(userInfo));
+    navigate(-1);
+  }
+  return( 
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg}/>
+        <Header>{'<MY/>'}RECIPE</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username.." required />
+          <StyledInput type="password" placeholder="password.." required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+
+    </LoginContainer>
+    );
+};
+
+export default Login;
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,7 +1,7 @@
 import {Outlet, Navigate} from 'react-router-dom';
 
 const PrivateRouter = () => {
-  const user = true;
+  const user = JSON.parse(sessionStorage.getItem('user'));
   return  user ? <Outlet/> : <Navigate to='/login'/>;
   
 };
