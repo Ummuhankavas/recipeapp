08fffe842e70a16799f132e1807c883b81c40077
UmmuhanKavas <ummuhankavas@gmail.com>
2022-11-04 11:08:50 +0100

    private router done

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import {Outlet, Navigate} from 'react-router-dom';
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
-}
+  const user = true;
+  return  user ? <Outlet/> : <Navigate to='/login'/>;
+  
+};
 
-export default PrivateRouter
+export default PrivateRouter;
