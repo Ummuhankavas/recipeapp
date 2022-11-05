f5cf97f95b9c9e8a2436357df9875d5254c53ffa
UmmuhanKavas <ummuhankavas@gmail.com>
2022-11-05 12:48:20 +0100

    navbar and navbar style finished

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,8 +1,31 @@
-import NavStyle from './Navbar.style';
-const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+import Nav, { Logo, Menu, MenuLink, Hamburger } from './Navbar.style';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from 'react';
 
-export default Nav
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">
+        <i>{'<Ummuhan/>'}</i>
+        <span>Recipe</span>
+      </Logo>
+
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="login" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
